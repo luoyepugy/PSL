5e45d31022b4bdec827ab3c1cd2eef8a317c561c
zhanghan <1119432095@qq.com>
2016-03-29 14:25:22 +0800

    导航响应式样式完成

--- www/js/contact.js
+++ www/js/contact.js
@@ -10,4 +10,26 @@ $(function() {
 		$(this).parents('.j-select').find('.j-selectInput').val($(this).text());
 	});
 
+	// 表单提交
+	$('.j-submit').click(function() {
+		var flag = true,
+			phone_regexp = /^((145|147)|(15[^4])|(17[6-8])|((13|18)[0-9]))\d{8}$/;
+			// email_regexp = 
+
+		$(this).closest('form').find('.j-input').each(function() {
+			var phone = $('.j-telephone').val();
+			if($(this).val().trim() == '') {
+				$(this).css('border-color','red');
+				flag = false;
+			}
+			if(phone_regexp.test(phone)) {
+				$(this).css('border-color','red');
+				flag = false;
+			}
+		});
+		if(flag) {
+			$(this).closest('form').submit();
+		}
+	});
+
 });
