1d33daf4a2fdac3cf91e51a08fc322dbbdae53ff
zhanghan <1119432095@qq.com>
2016-03-30 12:20:54 +0800

    首页banner图片替换

--- www/js/contact.js
+++ www/js/contact.js
@@ -2,12 +2,18 @@
 $(function() {
 
 	// 选择
-	$('.j-select').click(function() {
-		$(this).find('ul').toggleClass('none');
+	var index = 0;
+	$('.j-select').click(function(e) {
+		e.stopPropagation();
+		$(this).find('ul').toggleClass('none').closest('.j-select').siblings().find('ul').addClass('none');
+		index = $(this).index();
+	});
+	$('body').not('.j-select').click(function() {
+		$('form div').eq(index).find('ul').addClass('none');
 	});
 	$('.j-select li').click(function() {
 		$(this).parents('.j-select').find('.j-selectValue').text($(this).text());
-		$(this).parents('.j-select').find('.j-selectInput').val($(this).text());
+		$(this).parents('.j-select').find('.j-selectInput').val($(this).text());	
 	});
 
 	// 表单提交
@@ -17,14 +23,12 @@ $(function() {
 		var email = $('.j-email').val();
 
 		$(this).closest('form').find('.j-input').each(function() {
-			
 			if($(this).val().trim() == '') {
 				$(this).css('border-color','red');
 				flag = false;
 				return false;
 			}
-			console.log($(this).val());
-			
+			// console.log($(this).val());	
 		});
 		if(!RegExpHelper.isMobile(phone)) {
 			$('.j-telephone').css('border-color','red');
